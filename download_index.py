33d23feb41a0437d543dbeaf2fa06acbc23faaf3
Stefan Birkner <mail@stefan-birkner.de>
2018-05-14 10:42:52 +0200

    Don't download existing index files again
    If downloading the index files stops in the middle because of an error
    then a second run needed to download all files again. This needs a lot
    of time. The second run is now faster because existing index files are
    not downloaded again.

--- download_index.py
+++ download_index.py
@@ -6,13 +6,19 @@ from xml.dom.minidom import parse
 
 def collect(prefix):
     ensuredirs(prefix)
-    index = download(prefix)
+    index = ensure_index_file(prefix)
     for child_prefix in extract_children(index):
         collect(child_prefix)
 
 def ensuredirs(path):
     if not os.path.isdir(path):
         os.makedirs(path)
+
+def ensure_index_file(prefix):
+    path = prefix + "index.xml"
+    if not os.path.isfile(path):
+        download(prefix, path)
+    return path
 
 def extract_children(path):
     datasource = open(path)
@@ -23,9 +29,8 @@ def extract_children(path):
         in doc.getElementsByTagName("CommonPrefixes")
     ]
 
-def download(prefix):
+def download(prefix, path):
     url = "https://multimedia-commons.s3-us-west-2.amazonaws.com/?delimiter=/&prefix=" + prefix
-    path = prefix + "index.xml"
     print("Download " + url)
     urllib.request.urlretrieve (url, path)
     return path
